f647169b86a383ed721007be2025324d958a2d15
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-10-13 08:45:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective(visualFlair = '*'){
+    const innerFunction = function(adjective = 'hard worker'){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+    return innerFunction
+}
+
+wrapAdjective('||')('dedicated programmer')
